0f899764bcb53f70db6a9cab2326f97b40ea9885
SY-Mina <sylim9387@naver.com>
2021-08-21 08:53:38 +0000

    Update Structure

---
+++ src/main/java/com/example/demo/src/review/ReviewController.java
@@ -0,0 +1,57 @@
+package com.example.demo.src.review;
+
+import com.example.demo.config.BaseResponseStatus;
+import com.example.demo.src.user.model.GetUserRes;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.Serializers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.src.review.model.*;
+import com.example.demo.utils.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.List;
+
+
+import static com.example.demo.config.BaseResponseStatus.*;
+import static com.example.demo.utils.ValidationRegex.isRegexEmail;
+
+@RestController
+@RequestMapping("/app/reviews")
+public class ReviewController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private final ReviewProvider reviewProvider;
+    @Autowired
+    private final ReviewService reviewService;
+    @Autowired
+    private final JwtService jwtService;
+
+
+
+
+    public ReviewController(ReviewProvider reviewProvider, ReviewService reviewService, JwtService jwtService){
+        this.reviewProvider = reviewProvider;
+        this.reviewService = reviewService;
+        this.jwtService = jwtService;
+    }
+
+
+
+}
---
+++ src/main/java/com/example/demo/src/review/ReviewDao.java
@@ -0,0 +1,24 @@
+package com.example.demo.src.review;
+
+
+import com.example.demo.src.review.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ReviewDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    
+}
